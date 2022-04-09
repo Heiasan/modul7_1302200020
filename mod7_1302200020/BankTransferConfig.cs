24f9fb12f41902049d659f6610ff9096d4fb67dd
Heiasan <abdillahhasan130@gmail.com>
2022-04-09 18:12:32 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ mod7_1302200020/BankTransferConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod7_1302200020
+{
+    public class BankTransferConfig
+    {
+
+        public string lang { get; set; }
+
+        public transfers transfer { get; set; }
+        public string methods { get; set; }
+        public confirmation confirmation { get; set; }
+       
+
+    }
+    public class transfers 
+    {
+        public string threshold { get; set; }
+        public string low_fee { get; set; }
+        public string high_fee { get; set; }
+
+
+    }
+    class confirmation
+    {
+        public string en { get; set; }
+        public string id { get; set; }
+        public confirmation() { }
+        public confirmation(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+}
---
+++ mod7_1302200020/BankTransferController.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod7_1302200020
+{
+    public class BankTransferController
+    {
+        public void writejson(BankTransferConfig data) 
+        {
+            string file = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText("D://00_File Kampus//S4//KPL//PRAKTIKUM//TP KPL//mod7_1302200020//mod7_1302200020//BankTranfer.json", file);
+        }
+        public BankTransferConfig readjson()
+        {
+            dynamic data = File.ReadAllText("D://00_File Kampus//S4//KPL//PRAKTIKUM//TP KPL//mod7_1302200020//mod7_1302200020//BankTranfer.json");
+            BankTransferConfig config = JsonConvert.DeserializeObject<BankTransferConfig>(data);
+            return config;
+        }
+        
+    }
+}
